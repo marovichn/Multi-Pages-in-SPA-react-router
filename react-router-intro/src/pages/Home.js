d65eec4813542d2ce7ff035f68659f206146b399
marovichn <nikolamarovic.11@gmail.com>
2023-04-13 17:29:29 +0200

    using-router-links

--- react-router-intro/src/pages/Home.js
+++ react-router-intro/src/pages/Home.js
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Got to the <Link to="/products">products page.</Link>
+      </p>
+    </>
+  );
 };
 export default HomePage;
---
+++ react-router-intro/src/pages/Products.js
@@ -0,0 +1,4 @@
+const ProductsPage = () => {
+  return <h1>Products Page</h1>;
+};
+export default ProductsPage;
