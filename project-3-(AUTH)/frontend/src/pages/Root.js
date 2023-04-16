3b6e25d8ef5696ae1645232c3f4035dafc658682
marovichn <nikolamarovic.11@gmail.com>
2023-04-16 02:19:58 +0200

    sessions-protecting-frontend

--- project-3-(AUTH)/frontend/src/pages/Root.js
+++ project-3-(AUTH)/frontend/src/pages/Root.js
@@ -2,18 +2,27 @@ import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   const token = useLoaderData();
   const submit = useSubmit();
-  // const navigation = useNavigation();
+
   useEffect(() => {
     if (!token) {
       return;
     }
-    setTimeout(() => {
+
+    if (token === "EXPIRED") {
       submit(null, { action: "/logout", method: "post" });
-    }, 3600000);
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    if (token)
+      setTimeout(() => {
+        submit(null, { action: "/logout", method: "post" });
+      }, tokenDuration);
   }, [token, submit]);
 
   return (
--- project-3-(AUTH)/frontend/src/util/auth.js
+++ project-3-(AUTH)/frontend/src/util/auth.js
@@ -1,5 +1,24 @@
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = newDate(storedExpirationDate);
+  const now = new Date();
+  const tokenDuration = expirationDate.getTime() - now.getTime();
+  return tokenDuration;
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
